be1a5db4f48b47036da1ecf2b4fd7a352c761ed4
macc <165694744+noahskinner1@users.noreply.github.com>
2024-04-22 00:04:24 -0700

    Update and rename NumberServer.java to ChatServer.java

---
+++ ChatServer.java
@@ -0,0 +1,44 @@
+import java.io.IOException;
+import java.net.URI;
+
+class ChatHandler implements URLHandler {
+    // The single string that gets added to by incoming requests
+    String chatMessages = "";
+
+    public String handleRequest(URI url) {
+        if (url.getPath().equals("/add-message")) {
+            String query = url.getQuery();
+            String[] parameters = query.split("&");
+            String user = "";
+            String message = "";
+            for (String parameter : parameters) {
+                String[] keyValue = parameter.split("=");
+                if (keyValue[0].equals("s")) {
+                    message = keyValue[1];
+                } else if (keyValue[0].equals("user")) {
+                    user = keyValue[1];
+                }
+            }
+            if (!user.isEmpty() && !message.isEmpty()) {
+                chatMessages += user + ": " + message + "\n";
+                return chatMessages;
+            }
+        }
+        return "404 Not Found!";
+    }
+}
+
+
+class ChatServer {
+    public static void main(String[] args) throws IOException {
+        if (args.length == 0) {
+            System.out.println("Missing port number! Try any number between 1024 to 49151");
+            return;
+        }
+
+        int port = Integer.parseInt(args[0]);
+
+        Server.start(port, new Handler());
+    }
+}
+
